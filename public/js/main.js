09bfa915cc32dbe48aaae5b8f30054fde3b7adff
William Tome <william.tomenh@gmail.com>
2018-10-02 18:53:31 -0300

    Criada funcionalidade que realiza a contagem dinâmica de caracteres e palavras de um textarea.

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,11 @@ var frase = $(".frase").text();
 var numPalavras = frase.split(" ").length;
 var tamanhoFrase = $("#tamanho-frase");
 tamanhoFrase.text(numPalavras);
+
+var campo = $(".campo-digitacao")
+campo.on("input", function () { 
+    var conteudo = campo.val();
+    var qtdePalavras = conteudo.split(/\S+/).length - 1;
+    $("#contador-palavras").text(qtdePalavras);
+    $("#contador-caracteres").text(conteudo.length);
+});
